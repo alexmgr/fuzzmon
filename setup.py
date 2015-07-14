4a3c3f7797ab1c1e2c854b7cad1341691100f207
Alex Moneger <alexmgr+github@gmail.com>
2015-07-13 17:53:58 -0700

    Generate random session id by default
    - Bump setup.py version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 setup(
     name="fuzzmon",
-    version="0.1",
+    version="0.2",
     packages=["fuzz_proxy"],
     author="Alex Moneger",
     author_email="alexmgr+github@gmail.com",
     description=(
-        "A transport layer proxy which monitors the server backend"),
+        "A transport layer proxy which monitors the target server using ptrace"),
     license="GPLv2",
     keywords=["proxy", "fuzzing", "debugger", "ptrace", "bsd"],
     url="https://github.com/alexmgr/fuzzmon",
