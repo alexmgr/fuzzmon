d1fb30d9838d029514210332a437482cf9c1d1eb
Alex Moneger <alexmgr+github@gmail.com>
2016-03-18 11:14:25 -0700

    Updates for pip push
    - Converted MD to RST markdown

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
     name="fuzzmon",
@@ -11,11 +17,13 @@ setup(
     author_email="alexmgr+github@gmail.com",
     description=(
         "A transport layer proxy which monitors the target server using ptrace"),
-    license="GPLv2",
+    license="MIT",
     keywords=["proxy", "fuzzing", "debugger", "ptrace", "bsd"],
     url="https://github.com/alexmgr/fuzzmon",
     install_requires=["python-ptrace", "distorm3"],
     scripts=["fuzzmon", "fuzzreplay"],
+    # generate rst from .md:  pandoc --from=markdown --to=rst README.md -o README.rst
+    long_description=read("README.rst") if os.path.isfile("README.rst") else read("README.md")
     test_suite="nose.collector",
     tests_require=["nose"]
 )
